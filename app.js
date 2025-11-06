03e9359121603a5d715b3c8e650c1affa4c0ab18
Mateo <mateoriveira@gmail.com>
2025-11-06 13:34:23 -0300

    Punto Estable 3

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
+// app.js
 import express from "express";
 import productsRouter from "./products/productRouter.js";
 import cartsRouter from "./carts/cartRouter.js";
-
+import ProductManager from "./products/productManager.js";
 
 const app = express();
+const PORT = 8080;
+
+
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/thumbnails", express.static("thumbnails"));
+
+
+const productManager = new ProductManager("./data/products.json");
+app.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    return res.json(products);
+  } catch (err) {
+    return res.status(500).json({ error: "Error al leer products.json" });
+  }
+});
 
 
 app.use("/products", productsRouter);
 app.use("/carts", cartsRouter);
 
 
-
-
-const PORT = 8080;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/thumbnails", express.static("thumbnails"));
-
-
-
-
-app.get("/", (req, res) => res.send("Funcionando"));
-
-
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  });
+  
 
 export default app;
